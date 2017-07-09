4df22f6219281d2b4f0280914006dd2ecdef9f7a
elsr <elsr@list.ru>
2017-07-09 14:54:12 +0500

    Подключение дочерней темы в файле functions.php

---
+++ 5functions.php
@@ -0,0 +1,5 @@
+add_action( 'wp_enqueue_scripts', 'artabr_child_theme_enqueue_styles' );
+function artabr_child_theme_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), wp_get_theme()->get( 'Version' ) );
+}
